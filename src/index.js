019666094fb525e4ed778bdaa084f069fb703d27
slikts <dabas@untu.ms>
2020-02-23 23:41:37 +0000

    Init

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/** @jsx createValueElement */
+
+import { createElement } from "react";
+import { ValueObject } from "tuplerone";
+
+const keyFilter = ([key]) => !key.startsWith("_");
+export const createValueElement = (type, props, ...children) => {
+  const element = createElement(type, props, ...children);
+  if (typeof type === "string") {
+    return element;
+  }
+  return ValueObject(element, keyFilter);
+};
